3538065f5067b3f47d0a34451445249dc7e2d91b
Shoeb Shaikh <shoeb.shaikh@kgkmail.com>
2023-09-28 15:32:23 +0530

    action changes

--- .github/actions/setup.sh
+++ .github/actions/setup.sh
@@ -58,9 +58,10 @@ setup_flutter() {
     download_manifest
     
     [[ "$channel" == "master" ]] && FLUTTER_VERSION="master" || {
-        local version_manifest=$(filter_manifest "$channel" "$arch" "$version")
+        local version_manifest
+        version_manifest=$(filter_manifest "$channel" "$arch" "$version")
         [[ -n "$version_manifest" ]] || { echo "Unable to determine Flutter version."; exit 1; }
-        FLUTTER_VERSION="${version_manifest.version}"
+        FLUTTER_VERSION="$(echo "$version_manifest" | jq -r '.version')"
     }
     
     local archive_url="https://storage.googleapis.com/flutter_infra_release/releases/${FLUTTER_VERSION}/flutter_${OS_NAME}_${arch}.tar.xz"
